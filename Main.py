2ce7f20bb85f509bfd4062e940831e31a2289bc2
Ronnyn011 <48674898+Ronny011@users.noreply.github.com>
2020-02-27 04:05:27 +0200

    Update Main.py
    first third works

--- Main.py
+++ Main.py
@@ -1,12 +1,25 @@
 
-# get cookies - func logs in and pulls session cookie
-# fake cookie - func brute forces a possible cookie
-# try new cookie - func changes the file name or content and puts inside the directory
+#  [✓] get cookies - func logs in and pulls session cookie
+#  [ ] fake cookie - func brute forces a possible cookie
+#  [ ] try new cookie - func changes the file name or content and puts inside the directory
 
+from bs4 import BeautifulSoup as bs
 import requests
-session = requests.Session()
-response = session.get('http://localhost/login/new/index.php')
-# print(session.cookies.get_dict())
-for c in session.cookies:
-    print("\nSession cookie information:")
-    print("{" + 'name: ' + c.name + ', value :' + c.value + ', domain :' + c.domain + ', path: ' + c.path + "}")
+login_data = {
+    "username": 'liron',
+    "password": '3232',
+    "submit": " Login "
+}
+with requests.Session() as s:
+    url = "http://localhost/login/new/index.php"
+    r = s.post(url, data=login_data)
+    t = s.get(url)
+    soup = bs(r.content, 'html.parser')
+    soup = bs(t.content, 'html.parser')
+    print("\n")
+    flag = 1
+    #print(soup)
+    print(r.url)
+    print(t.url)
+    #str1 =
+    #print(s.cookies)
